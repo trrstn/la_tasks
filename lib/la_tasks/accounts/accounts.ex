2b8506462e682a2e45ed8de16b3f921a41aef619
Tristan Mallorca <trrstnm@gmail.com>
2026-03-19 19:32:48 +0800

    Moves changesets to schema

--- lib/la_tasks/accounts/accounts.ex
+++ lib/la_tasks/accounts/accounts.ex
@@ -6,61 +6,34 @@ defmodule LaTasks.Accounts do
   """
   alias LaTasks.Accounts.User
   alias LaTasks.Repo
-  import Ecto.Changeset
 
   ## Mutations
   def register_user(attrs) do
     %User{}
-    |> create_changeset(attrs)
+    |> User.create_changeset(attrs)
     |> Repo.insert()
   end
 
-  ## Changesets and utils
-  defp create_changeset(user, attrs) do
-    user
-    |> cast(attrs, [:username, :password, :password_confirmation])
-    |> update_change(:username, &normalize_username/1)
-    |> validate_required([:username, :password, :password_confirmation])
-    |> validate_length(:username, min: 3, max: 20)
-    |> validate_format(
-      :username,
-      ~r/^[a-z0-9]+(-[a-z0-9]+)*$/,
-      message:
-        "must contain only lowercase letters, numbers, and single hyphens; it cannot start or end with a hyphen"
-    )
-    |> unique_constraint(:username, message: "username has already been taken")
-    |> validate_password()
-    |> put_password_hash()
+  # Queries
+  def get_user_by_username(username) when is_binary(username) do
+    username = User.normalize_username(username)
+    Repo.get_by(User, username: username)
   end
 
-  defp normalize_username(username) do
-    username
-    |> String.trim()
-    |> String.downcase()
-  end
-
-  defp validate_password(changeset) do
-    changeset
-    |> validate_length(:password, min: 8, message: "password should be at least 8 characters")
-    |> validate_format(:password, ~r/[[:punct:]]/,
-      message: "password must contain at least one special character"
-    )
-    |> validate_format(:password, ~r/[A-Z]+/,
-      message: "password must contain an uppercase letter"
-    )
-    |> validate_confirmation(:password)
-  end
-
-  defp put_password_hash(%Ecto.Changeset{valid?: true} = changeset) do
-    case get_change(changeset, :password) do
+  ## Utils
+  def authenticate_user(username, password)
+      when is_binary(username) and is_binary(password) do
+    case get_user_by_username(username) do
       nil ->
-        changeset
+        Argon2.no_user_verify()
+        {:error, :invalid_credentials}
 
-      password ->
-        hash = Argon2.hash_pwd_salt(password)
-        put_change(changeset, :hashed_password, hash)
+      user ->
+        if Argon2.verify_pass(password, user.hashed_password) do
+          {:ok, user}
+        else
+          {:error, :invalid_credentials}
+        end
     end
   end
-
-  defp put_password_hash(changeset), do: changeset
 end
--- lib/la_tasks/accounts/user.ex
+++ lib/la_tasks/accounts/user.ex
@@ -1,5 +1,6 @@
 defmodule LaTasks.Accounts.User do
   use Ecto.Schema
+  import Ecto.Changeset
 
   schema "users" do
     field :username, :string
@@ -9,4 +10,52 @@ defmodule LaTasks.Accounts.User do
 
     timestamps(type: :utc_datetime)
   end
+
+  def create_changeset(user, attrs) do
+    user
+    |> cast(attrs, [:username, :password, :password_confirmation])
+    |> update_change(:username, &normalize_username/1)
+    |> validate_required([:username, :password, :password_confirmation])
+    |> validate_length(:username, min: 3, max: 20)
+    |> validate_format(
+      :username,
+      ~r/^[a-z0-9]+(-[a-z0-9]+)*$/,
+      message:
+        "must contain only lowercase letters, numbers, and single hyphens; it cannot start or end with a hyphen"
+    )
+    |> unique_constraint(:username, message: "username has already been taken")
+    |> validate_password()
+    |> put_password_hash()
+  end
+
+  def normalize_username(username) do
+    username
+    |> String.trim()
+    |> String.downcase()
+  end
+
+  defp validate_password(changeset) do
+    changeset
+    |> validate_length(:password, min: 8, message: "password should be at least 8 characters")
+    |> validate_format(:password, ~r/[[:punct:]]/,
+      message: "password must contain at least one special character"
+    )
+    |> validate_format(:password, ~r/[A-Z]+/,
+      message: "password must contain an uppercase letter"
+    )
+    |> validate_confirmation(:password)
+  end
+
+  defp put_password_hash(%Ecto.Changeset{valid?: true} = changeset) do
+    case get_change(changeset, :password) do
+      nil ->
+        changeset
+
+      password ->
+        hash = Argon2.hash_pwd_salt(password)
+        put_change(changeset, :hashed_password, hash)
+    end
+  end
+
+  defp put_password_hash(changeset), do: changeset
 end
